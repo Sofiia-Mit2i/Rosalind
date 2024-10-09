06ec67bfba031bf146314dd1f91ee56a6cc9e324
Ilnytska, Sofiia <sofiia.ilnytska@bih-charite.de>
2024-10-09 13:29:24 +0200

    FiboNum

--- AlgorithmicHeights/FibonacciNumbers.py
+++ AlgorithmicHeights/FibonacciNumbers.py
@@ -1,1 +1,14 @@
+def Fibonacci (n):
+    if n <= 25:
+       numbers = [0,1]
+       while len(numbers) <= n:
+             i = numbers[-2] + numbers[-1]
+             numbers.append(i)
+       result = numbers[-1]
+       print(f'Your Fibonacci Number is: {result}')
+    else:
+        print("Your number has to be less than 25. ")
+
+n = int(input("Enter your number: "))
+Fibonacci(n)
 
